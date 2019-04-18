5ac7c59cbb4bad8154587a2548752d06660bb598
Adam Shaw <ashaw@osisoft.com>
2019-04-18 09:18:21 -0700

    Remove Obsolete Packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 var gulp = require('gulp');
-var jsHint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
 var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
-
-gulp.task('style', function() {
-    return gulp.src(jsFiles)
-        .pipe(jsHint())
-        .pipe(jsHint.reporter('jshint-stylish' , {
-            verbose: true
-        }))
-        .pipe(jscs());
-});
 
 gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
@@ -36,7 +25,7 @@ gulp.task('inject', function() {
 /* When the 'gulp serve' command is run, the application starts
    saved changes to any of the jsFiles files will trigger the application
    to restart so the page can be refreshed in the browser to see changes */
-gulp.task('serve', gulp.series(gulp.parallel('style', 'inject'), function() {
+gulp.task('serve', gulp.series(gulp.parallel('inject'), function() {
     var options = {
         script: 'app.js',
         delayTime: 1,
